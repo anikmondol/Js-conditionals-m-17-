8dcdc40dd125f598b6be16f69e47839910d6cc50
anikmondol <anikmondol558363@gmil.com>
2024-03-14 01:17:25 +0600

    introduction to else conditon

---
+++ if.js
@@ -0,0 +1,72 @@
+/**
+ * tui jodi amake khawas 
+ *      taile ami asbo
+ * 
+ * 
+ * 
+ * ------------------------
+ * 
+ * if you buy food for me 
+ *      i will come
+ * 
+ * 
+ * 
+ * ------------------------
+ * 
+ * if briyani price less than 300 taka 
+ *      i will buy
+ * 
+ * -------------STRUCTURE--------------
+ * 
+ * if (condition){
+ * 
+ * }
+ * 
+ * 
+ * --------------------------------------
+ * 
+ * if (condition){
+ * 
+ * }
+ * 
+ * */ 
+
+
+// if (3 < 10){
+//     console.log('i have a smaller value');
+// }
+
+
+// var briyaniPrice = 250;
+
+// if(briyaniPrice < 300){
+//     console.log('mama give me some biryani');
+// }
+
+
+
+/**
+ * 
+ * if (condition){
+ *      execute code if condition above is true
+ * }else{
+ *      execute code if the condition is false 
+ * }
+ * 
+ * */ 
+
+
+// var weight = 10;
+
+// if(weight < 20){
+//     console.log('I will carry it by myself');
+// }else{
+//     console.log('I will rent a rickshaw');
+// }
+
+
+let a = 2;
+
+if(a <= 5){
+    console.log('if statement executed');
+}
