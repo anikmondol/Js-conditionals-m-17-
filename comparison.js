6830246c0729c22736454a488e682efba807aa57
anikmondol <anikmondol558363@gmil.com>
2024-03-14 00:37:20 +0600

    introduction to conditionals js

---
+++ comparison.js
@@ -0,0 +1,46 @@
+// console.log(5 > 10);
+// console.log(5 < 10);
+// console.log(5 == 10);
+// console.log(10 == 10);
+// console.log(15 >= 10);
+// console.log(5 >= 10);
+// console.log(5 <= 10);
+// console.log(10 <= 10);
+// console.log(10 <= 10);
+
+
+// console.log(10 == 10);
+// console.log(10 == 10.1);
+
+
+
+// ------------------------
+
+
+var peyaraPrice = 40;
+var applePrice = 240;
+
+
+// console.log(peyaraPrice > applePrice);
+// console.log(applePrice < peyaraPrice);
+// console.log(applePrice > peyaraPrice);
+// console.log(peyaraPrice <= applePrice);
+
+
+
+/**
+ * ==
+ * ===
+ * */ 
+
+
+
+// console.log(10 == 10);
+// console.log(10 == "10");
+// console.log(10 === '10');
+
+
+//  -----------
+
+console.log(10 != 10);
+console.log(10 !== '10'); 
