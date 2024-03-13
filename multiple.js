e7ef3ca096ef4481ae0a4307674f2a256c684d57
anikmondol <anikmondol558363@gmil.com>
2024-03-14 01:38:27 +0600

    multiple condition logical operators

---
+++ multiple.js
@@ -0,0 +1,53 @@
+const salary = 25001;
+const isBcs = true;
+const height = 68;
+const hasCar = false;
+
+// if(salary > 2000 && height > 66){
+//     console.log("Su---patro");
+// }else{
+//     console.log('onno parto khuji');
+// }
+
+
+// if(salary > 25000 || height > 70){
+//     console.log('eso baba kobul');
+// }else{
+//     console.log("vag toi mokbul");
+// }
+
+
+// more and more condition 
+
+
+
+// if(salary > 25000 || height > 72 || isBcs == true){
+//     console.log('eso baba kobul');
+// }else{
+//     console.log('vag tui mokbul');
+// }
+
+
+
+
+// if(salary > 25000 && height > 72 && isBcs == true){
+//     console.log('eso baba kobul');
+// }else{
+//     console.log('vag tui mokbul');
+// }
+
+
+
+// ----------complex condition-------------
+
+
+if((salary > 25000 || hasCar == true) && isBcs == true){
+    console.log('tomar 14 gosti raji');
+}else{
+    console.log('vaag tui mokbul');
+}
+// if((salary > 25000 && hasCar == true) || isBcs == false){
+//     console.log('tomar 14 gosti raji');
+// }else{
+//     console.log('vaag tui mokbul');
+// }
