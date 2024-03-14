863d7c48835d96cbf6f8c8c487c0c446261d199d
anikmondol <anikmondol558363@gmil.com>
2024-03-14 14:05:02 +0600

    not operator

---
+++ not-operator.js
@@ -0,0 +1,37 @@
+const isLeader = true;
+
+
+// if(isLeader === true)
+
+
+
+// if(isLeader === true){
+//     console.log('leader asche! chair charo');
+// }else{
+//     console.log('vitore dukte diba na');
+// }
+
+
+
+// if(isLeader){
+//     console.log('leader asche! chair charo');
+// }else{
+//     console.log('vitore dukte diba na');
+// }
+
+
+
+const isPassed = false
+
+
+// if(isLeader === false)
+if(!isPassed ){
+    console.log('toke biya diya demo');
+}else{
+    console.log('tumei amader noyon er moni');
+}
+// if(isPassed === false){
+//     console.log('toke biya diya demo');
+// }else{
+//     console.log('tumei amader noyon er moni');
+// }
