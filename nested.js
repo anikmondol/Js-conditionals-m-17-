679e7eaa70615edcaf27029165955322efa7c0dd
anikmondol <anikmondol558363@gmil.com>
2024-03-14 13:01:39 +0600

    nested if-else condition

---
+++ nested.js
@@ -0,0 +1,16 @@
+const money = 0;
+
+
+if(money > 300){
+    console.log('bro you are rich!!');
+}else{
+    if(money > 100){
+        console.log('tui gorib o na boro lok oo na');
+    }else{
+        if(money > 0){
+            console.log('tumei kola khoi ga');
+        }else{
+            console.log('tumer sateay katha nai');
+        }
+    }
+}
